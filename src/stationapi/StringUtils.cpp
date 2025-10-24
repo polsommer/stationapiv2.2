b97b0b7db26335528429aaab2bddc997ebd26166
polsommer <patricker@live.dk>
2025-10-24 18:51:58 +0200

    Revert "Merge pull request #10 from polsommer/codex/add-utf-8-to-std-u16string-helper"
    This reverts commit b7dd89f3afe6ac2140e65b2a66ea24fba2a47ae2, reversing
    changes made to 5825fcd8cd5c3d7cee1140343d40a33fd818eb60.

--- src/stationapi/StringUtils.cpp
+++ src/stationapi/StringUtils.cpp
@@ -1,24 +1,10 @@
-#include "StringUtils.hpp"
 
-#include <codecvt>
-#include <locale>
-#include <optional>
+#include "StreamUtils.hpp"
 
 std::string FromWideString(const std::u16string& str) {
-    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> converter;
-    return converter.to_bytes(str);
+    return std::string{std::begin(str), std::end(str)};
 }
 
 std::u16string ToWideString(const std::string& str) {
-    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> converter;
-    return converter.from_bytes(str);
-}
-
-std::optional<std::u16string> NullableUtf8ToWide(const unsigned char* buffer) {
-    if (!buffer) {
-        return std::nullopt;
-    }
-
-    const auto utf8 = reinterpret_cast<const char*>(buffer);
-    return ToWideString(utf8);
+    return std::u16string{std::begin(str), std::end(str)};
 }
